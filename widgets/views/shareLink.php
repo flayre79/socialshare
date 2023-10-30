a819ee581b066e170b2dbb95c8274e1d757963f3
flayre79 <39012464+flayre79@users.noreply.github.com>
2023-10-30 13:54:46 -0400

    Add files via upload
    Begin work to add options for Instagram, Bluesky, Mastodon, Threads, and removing Line.me. Also updated Twitter to reflect new name.

--- widgets/views/shareLink.php
+++ widgets/views/shareLink.php
@@ -26,9 +26,14 @@ Assets::register($this);
 
 			";
 		?>
-        	<?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode((string)$permalink) . '&description=' . urlencode((string)$object->getContentDescription()), ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink) . '&ro=false', ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-share" style="font-size:16px;color:#00c300">&nbsp;</i>', 'https://social-plugins.line.me/lineit/share?' . '&text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode((string)$permalink) . '&description=' . urlencode((string)$object->getContentDescription()), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-x" style="font-size:16px;color:#newColor">&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink) . '&ro=false', ['onclick' => $option]); ?>
+
+    	<?= Html::a('<i class="fa fa-threads" style="font-size:16px;color:#threadsColor">&nbsp;</i>', 'https://threads.com/share?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-instagram" style="font-size:16px;color:#instagramColor">&nbsp;</i>', 'https://www.instagram.com/share?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-bluesky" style="font-size:16px;color:#blueskyColor">&nbsp;</i>', 'https://bluesky.com/share?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+    	<?= Html::a('<i class="fa fa-mastodon" style="font-size:16px;color:#mastodonColor">&nbsp;</i>', 'https://mastodon.social/share?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
 	</div>
 </span>
+
